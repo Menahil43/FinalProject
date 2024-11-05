f8f500350655f6487fa8ee3d23addba4ad059c8e
Menahil Faiz <menahil4310@gmail.com>
2024-11-04 22:46:03 -0800

    Describe the changes you made

--- src/main/java/com/chatbuddy/chat_application/config/ChatApplicationEventListener.java
+++ src/main/java/com/chatbuddy/chat_application/config/ChatApplicationEventListener.java
@@ -1,5 +1,38 @@
 package com.chatbuddy.chat_application.config;
 
-public class ChatApplicationEventListener {
+import java.util.Objects;
 
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.chatbuddy.chat_application.service.Message;
+import com.chatbuddy.chat_application.service.MsgType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ChatApplicationEventListener {
+    
+    private final SimpMessageSendingOperations messageOperations; // Line 22
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        
+        if (Objects.nonNull(username)) {
+            log.info("User disconnected: {}", username);
+            
+            messageOperations.convertAndSend("/topic/chat", Message.builder() // Line 30
+                .type(MsgType.LEAVE) // Line 32
+                .sender(username)
+                .build());
+        }
+    }
 }
--- src/main/java/com/chatbuddy/chat_application/service/Message.java
+++ src/main/java/com/chatbuddy/chat_application/service/Message.java
@@ -2,10 +2,11 @@ package com.chatbuddy.chat_application.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+@Data
 @Getter
 @Setter
 @AllArgsConstructor
